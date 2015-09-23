542359084c03bfe5c88f84061a23455a4ac30d75
nrgs <nzare1@cougars.ccis.edu>
2015-09-23 09:08:59 -0500

    You can move it alittle bit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 
-#include "SDL.h"
+#include <SDL.h>
 #include <SDL_image.h>
+#include <stdio.h>
+#include <string>
 
 int main(int argc, char* argv[])
 {
@@ -9,6 +11,7 @@ int main(int argc, char* argv[])
     SDL_Surface *shipImage = NULL;
     SDL_Surface *backgroundImage = NULL;
     SDL_Rect rect;
+    SDL_Renderer* renderer = NULL;
 
     SDL_Init(SDL_INIT_VIDEO); 
 
@@ -21,42 +24,67 @@ int main(int argc, char* argv[])
     bool quit = false;
 
     /* set sprite position */
-    int positionX = 200;
-    int positionY = 200;
-    rect.x = positionX;
-    rect.y = positionY;
+    rect.x = 200;
+    rect.y = 200;
     if(window != NULL)
     {
         backgroundImage = IMG_Load("background.png"); //Load Background
         shipImage = IMG_Load("unicorn2.png"); // loads image
+
+       // SDL_BlitSurface(backgroundImage, NULL, screen, NULL); 
        
         SDL_Event event;
         while(!quit)
         {
             
-            if(SDL_PollEvent(&event))
+            while(SDL_PollEvent(&event) != 0)
             {
-                if(event.type == SDL_QUIT)
+                //User requests quit
+                switch(event.type)
                 {
-                    quit = true;
+                    case SDL_QUIT:
+                            quit = true;
+                            break;
+
+                    case SDL_KEYDOWN:
+                            rect.y -= 10;
+                            break;
+                    case SDL_KEYUP:
+                            rect.y += 10;
+                            break;       
                 }
-                if(event.type == SDL_KEYDOWN)
-                {
-                    positionY -= 10;
-                }
-                if(event.type == SDL_KEYUP)
-                {
-                    positionY += 10;
-                }
+                // if(event.type == SDL_QUIT)
+                // {
+                //     quit = true;
+                // }
+                // else if(event.type == SDL_KEYDOWN)
+                // {
+                //     rect.y += 10;
+                // }
+                // else if(event.type == SDL_KEYUP)
+                // {
+                //     rect.y -= 10;
+                // }
+
             }
 
+            //Clear screen
+           // SDL_RenderClear(renderer);
 
-            
-        
+            //Apply the images
             SDL_BlitSurface(backgroundImage, NULL, screen, NULL); 
             SDL_BlitSurface(shipImage, NULL, screen, &rect); 
+            
+            //Update screen
+            //SDL_RenderPresent(renderer);
+            //Update the surface
             SDL_UpdateWindowSurface(window);
+  
+            
         }
+
+        SDL_FreeSurface(backgroundImage);
+        SDL_FreeSurface(shipImage);
     }
     SDL_DestroyWindow(window);
     SDL_Quit();
